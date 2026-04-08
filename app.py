1ff29e0448d5193f1c8f1d145b142c811d2bf463
byzhgdot2 <bran.zhg26@gmail.com>
2026-04-08 10:07:44 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("HELLO EVERYONE")
+st.markdown(
+    """
+    boom boom boom 
+
+    etc etc etc
+
+    etc etc etc
+    """
+)
+
+if st.buttons("Send Balloons!"):
+    st.balloons()
