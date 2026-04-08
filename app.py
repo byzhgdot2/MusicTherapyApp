577f68be8195f6d2cfb03e29818c14be992727b7
byzhgdot2 <bran.zhg26@gmail.com>
2026-04-08 10:14:14 -0400

    Change title and update markdown in app.py
    Updated title and modified markdown content.

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import streamlit as st
 
-st.title("Hello Streamlit-er 👋")
+st.title("Random bs!")
 st.markdown(
     """ 
-    This is a playground for you to try Streamlit and have fun. 
+    hello
 
-    **There's :rainbow[so much] you can build!**
-    
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
+    hello
+    hleoo
+
+    hello
     """
 )
 
