59dbf2d09ad7e552225b22eae46620fc53318b49
byzhgdot2 <bran.zhg26@gmail.com>
2026-04-13 10:26:55 -0400

    Refactor sidebar input methods in appproto.py
    Refactor sidebar elements to use object and 'with' notation.

--- appproto.py
+++ appproto.py
@@ -15,7 +15,18 @@ st.markdown(
     """
 )
 
-st.sidebar.["BOOM"]
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
 
 uploaded = st.file_uploader("Upload YOUR physiological signals here! (.csv)", type="csv")
 
